d374b97d7b73500329345238b358a2bad31b9321
YewMun Yip <yipy@9D6LM44.dhcp.thecrick.org>
2025-03-29 22:33:48 +0000

    feat: Add the Explore Chemical Space app

--- cheminformatics_and_molecular_property_prediction/template.py
+++ cheminformatics_and_molecular_property_prediction/template.py
@@ -2,6 +2,6 @@ import sys
 
 sys.path.append("app_Explore-Chemical-Space")
 
-from app_Explore_Chemical_Space.main import main  # type: ignore
+from app_Explore-Chemical-Space.main import main  # type: ignore
 
 main()
