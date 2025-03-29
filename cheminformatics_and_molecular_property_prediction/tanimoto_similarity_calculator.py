2b128c53a7b18770971c2aa96cf1cc25c389efb8
YewMun Yip <yipy@9D6LM44.dhcp.thecrick.org>
2025-03-29 22:46:42 +0000

    feat: Add the Tanimoto Similarity Calculator app

--- cheminformatics_and_molecular_property_prediction/tanimoto_similarity_calculator.py
+++ cheminformatics_and_molecular_property_prediction/tanimoto_similarity_calculator.py
@@ -1,1 +1,7 @@
-# Empty script for Tanimoto Similarity Calculator
+import sys
+
+sys.path.append("app_Tanimoto_Similarity_Calculator")
+
+from app_Tanimoto_Similarity_Calculator.main import main  # type: ignore
+
+main()
