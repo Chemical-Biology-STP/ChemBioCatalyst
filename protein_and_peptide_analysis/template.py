be38a4e72c55bd182c15926554cf1c0f1420e5ea
YewMun Yip <yipy@9D6LM44.dhcp.thecrick.org>
2025-03-29 23:41:13 +0000

    feat: Add the Transpose Peptide Sequences tool

--- protein_and_peptide_analysis/template.py
+++ protein_and_peptide_analysis/template.py
@@ -1,7 +1,7 @@
 import sys
 
-sys.path.append("app_ProtParam")
+sys.path.append("app_Transpose_Peptide_Sequences")
 
-from app_ProtParam.main import main  # type: ignore
+from app_Transpose_Peptide_Sequences.main import main  # type: ignore
 
 main()
--- protein_and_peptide_analysis/transpose_peptide_sequences.py
+++ protein_and_peptide_analysis/transpose_peptide_sequences.py
@@ -1,1 +1,7 @@
-# Empty script for Transpose Peptide Sequences
+import sys
+
+sys.path.append("app_Transpose_Peptide_Sequences")
+
+from app_Transpose_Peptide_Sequences.main import main  # type: ignore
+
+main()
